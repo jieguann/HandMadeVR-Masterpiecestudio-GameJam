2a46b94afa38463e5631f3b4a81364bc1bc5a343
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-26 17:07:54 -0400

    set light map

--- GrabbalbeObject/Assets/Scripts/Target.cs
+++ GrabbalbeObject/Assets/Scripts/Target.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public float moveSpeed;
+    public float moveAmount;
+
+    private Vector3 StartPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var newPosition = transform.position;
+
+        newPosition.x = StartPosition.x + Mathf.Sin(Time.time * moveSpeed) * moveAmount;
+
+        transform.position = newPosition;
     }
 }
