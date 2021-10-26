788cb8cf532b9731e30ba5732af856470b15433a
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-26 17:33:22 -0400

    Update GameManager.cs

--- GrabbalbeObject/Assets/Scripts/GameManager.cs
+++ GrabbalbeObject/Assets/Scripts/GameManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject targetPrefeb;
+    private BoxCollider box;
+    private void SpawnTarget()
+    {
+        var newTarget = Instantiate(targetPrefeb, GenerateRandomPosition(),targetPrefeb.transform.rotation);
+    }
+
+    private Vector3 GenerateRandomPosition()
+    {
+        var randomPosition = new Vector3(
+            Random.Range(box.bounds.min.x, box.bounds.max.x), 
+            Random.Range(box.bounds.min.y, box.bounds.max.y), 
+            Random.Range(box.bounds.min.z, box.bounds.max.z));
+
+        return randomPosition;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        box = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
