0ace5a79d3b05873996bc8150ad07a23e09b8769
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-26 19:32:07 -0400

    update

--- GrabbalbeObject/Assets/Scripts/SnappableObject.cs
+++ GrabbalbeObject/Assets/Scripts/SnappableObject.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class SnappableObject : ThrowableObject
 {
     private bool isWithinSnappableAre = false;
-
+    private float snapZ;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("snap"))
         {
             isWithinSnappableAre = true;
+            snapZ = other.transform.position.z;
         }
     }
 
@@ -19,6 +20,20 @@ public class SnappableObject : ThrowableObject
         if (other.CompareTag("snap"))
         {
             isWithinSnappableAre = false;
+        }
+    }
+
+    public override void OnGrabEnd()
+    {
+        base.OnGrabEnd();
+
+        if (isWithinSnappableAre)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().useGravity = false;
+
+            var tempPosition = new Vector3(transform.position.x, transform.position.y, snapZ);
+            transform.position = tempPosition;
         }
     }
 
